8996d54d011719fff6417dad26162ea32df1273a
rinoj <jeezybox@gmail.com>
2021-05-28 15:08:04 +0200

    migrations

---
+++ database/migrations/2021_05_28_130337_create_produktet_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProduktetTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produktet', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('pershkrimi')->nullable();
+            $table->double('qmimi');
+            $table->integer('kategoria_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produktet');
+    }
+}
